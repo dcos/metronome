a905ce68af18add3ff751a5cf6456e5a13e69a19
Gastón Kleiman <gkleiman@users.noreply.github.com>
2016-05-30 18:07:41 +0200

    Install protoc in Travis CI build (#10)

---
+++ bin/install-protobuf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+# check to see if protobuf folder is empty
+if [ ! -d "$HOME/protobuf/lib" ]; then
+  wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
+  tar -xzvf protobuf-2.6.1.tar.gz
+  cd protobuf-2.6.1 && ./configure --prefix=$HOME/protobuf && make && make install
+else
+  echo "Using cached protobuf."
+fi
