cb17dd68ec6db966fe43d51655c6baf46af57595
Ken Sipe <kensipe@gmail.com>
2016-11-17 19:38:12 -0600

    si tests (#104)
    * adding si tests for jobs
    
    * pep8 cleanup
    
    * simplifying code based on pr feedback
    
    * fixing double entry issue.  it appears to have worked but was coded incorrectly
    
    * reverting back

---
+++ tests/system/common.py
@@ -0,0 +1,25 @@
+
+
+def job_no_schedule(id='pikachu', cmd='sleep 10000'):
+    return {
+        'id': id,
+        'description': 'electrifying rodent',
+        'run': {
+            'cmd': cmd,
+            'cpus': 0.01,
+            'mem': 32,
+            'disk': 0
+        }
+    }
+
+
+def schedule():
+    return {
+        "concurrencyPolicy": "ALLOW",
+        "cron": "20 0 * * *",
+        "enabled": True,
+        "id": "nightly",
+        "nextRunAt": "2016-07-26T00:20:00.000+0000",
+        "startingDeadlineSeconds": 900,
+        "timezone": "UTC"
+    }
---
+++ tests/system/test_root_metronome.py
@@ -0,0 +1,144 @@
+"""Metronome 'Job' System Integration Tests"""
+
+from common import *
+import contextlib
+from shakedown import *
+from dcos import *
+
+import metronome
+import time
+import uuid
+
+
+def test_add_job():
+    client = metronome.create_client()
+    with job(job_no_schedule()):
+        job_id = job_no_schedule()['id']
+        response = client.get_job(job_id)
+        assert response.get('id') == job_id
+
+
+def test_remove_job():
+    client = metronome.create_client()
+    client.add_job(job_no_schedule('remove-job'))
+    assert client.remove_job('remove-job') is None
+    job_exists = False
+    try:
+        client.get_job('remove-job')
+        job_exists = True
+    except:
+        pass
+    assert not job_exists, "Job exists"
+
+
+def test_list_jobs():
+    client = metronome.create_client()
+    with job(job_no_schedule('job1')):
+        with job(job_no_schedule('job2')):
+            jobs = client.get_jobs()
+            assert len(jobs) == 2
+
+
+def test_update_job():
+    client = metronome.create_client()
+    job_json = job_no_schedule('update-job')
+    with job(job_json):
+        assert client.get_job('update-job')['description'] == 'electrifying rodent'
+
+        job_json['description'] = 'updated description'
+        client.update_job('update-job', job_json)
+        assert client.get_job('update-job')['description'] == 'updated description'
+
+
+def test_add_schedule():
+    client = metronome.create_client()
+    with job(job_no_schedule('schedule')):
+        client.add_schedule('schedule', schedule())
+        assert client.get_schedule('schedule', 'nightly')['cron'] == '20 0 * * *'
+
+
+def test_update_schedule():
+    client = metronome.create_client()
+    with job(job_no_schedule('schedule')):
+        client.add_schedule('schedule', schedule())
+        assert client.get_schedule('schedule', 'nightly')['cron'] == '20 0 * * *'
+        schedule_json = schedule()
+        schedule_json['cron'] = '10 0 * * *'
+        client.update_schedule('schedule', 'nightly', schedule_json)
+        assert client.get_schedule('schedule', 'nightly')['cron'] == '10 0 * * *'
+
+
+def test_run_job():
+    client = metronome.create_client()
+    job_id = uuid.uuid4().hex
+    with job(job_no_schedule(job_id)):
+        runs = client.get_runs(job_id)
+        assert len(runs) == 0
+
+        client.run_job(job_id)
+        time.sleep(2)
+        assert len(client.get_runs(job_id)) == 1
+
+
+def test_get_job_run():
+    client = metronome.create_client()
+    job_id = uuid.uuid4().hex
+    with job(job_no_schedule(job_id)):
+        client.run_job(job_id)
+        time.sleep(2)
+        run_id = client.get_runs(job_id)[0]['id']
+        run = client.get_run(job_id, run_id)
+        assert run['id'] == run_id
+        assert run['status'] == 'ACTIVE'
+
+
+def test_stop_job_run():
+    client = metronome.create_client()
+    job_id = uuid.uuid4().hex
+    with job(job_no_schedule(job_id)):
+        client.run_job(job_id)
+        time.sleep(2)
+        assert len(client.get_runs(job_id)) == 1
+        run_id = client.get_runs(job_id)[0]['id']
+        client.kill_run(job_id, run_id)
+
+        assert len(client.get_runs(job_id)) == 0
+
+
+def test_remove_schedule():
+    client = metronome.create_client()
+    with job(job_no_schedule('schedule')):
+        client.add_schedule('schedule', schedule())
+        assert client.get_schedule('schedule', 'nightly')['cron'] == '20 0 * * *'
+        client.remove_schedule('schedule', 'nightly')
+        schedule_exists = False
+        try:
+            client.get_schedule('schedule', 'nightly')
+            schedule_exists = True
+        except:
+            pass
+        assert not schedule_exists, "Schedule exists"
+
+
+def remove_jobs():
+    client = metronome.create_client()
+    for job in client.get_jobs():
+        client.remove_job(job['id'])
+
+
+def setup_module(module):
+    agents = get_private_agents()
+    if len(agents) < 1:
+        assert False, "Incorrect Agent count"
+    remove_jobs()
+
+
+@contextlib.contextmanager
+def job(job_json):
+    job_id = job_json['id']
+    client = metronome.create_client()
+    client.add_job(job_json)
+    try:
+        yield
+    finally:
+        client.remove_job(job_id, True)
