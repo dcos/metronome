81a8d008e1e697abc92856b67a732f66c2ae91bc
Matthias Veit <aquamatthias@users.noreply.github.com>
2017-01-26 14:05:30 +0100

    Remove FORBID and REPLACE from the code as well as documentation (Fixes #100) (#121)
    The default value is set to ALLOW.
    The schema will make sure, only ALLOW is allowed.

--- jobs/src/main/scala/dcos/metronome/model/ConcurrencyPolicy.scala
+++ jobs/src/main/scala/dcos/metronome/model/ConcurrencyPolicy.scala
@@ -3,13 +3,9 @@ package dcos.metronome.model
 sealed trait ConcurrencyPolicy
 object ConcurrencyPolicy {
   case object Allow extends ConcurrencyPolicy
-  case object Forbid extends ConcurrencyPolicy
-  case object Replace extends ConcurrencyPolicy
 
   val names: Map[String, ConcurrencyPolicy] = Map(
-    "ALLOW" -> Allow,
-    "FORBID" -> Forbid,
-    "REPLACE" -> Replace
+    "ALLOW" -> Allow
   )
   val concurrencyPolicyNames: Map[ConcurrencyPolicy, String] = names.map{ case (a, b) => (b, a) }
 
