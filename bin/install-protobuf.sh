f9ac04cb1b68bde88ec22fb2937867c81c09a920
Ken Sipe <kensipe@gmail.com>
2017-12-19 14:34:48 -0600

    switching to marathon version of protobuf

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -2,9 +2,9 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  curl -L -O https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
-  tar -xzvf protobuf-2.6.1.tar.gz
-  cd protobuf-2.6.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  curl -L -o protobuf-3.3.0.tar.gz https://github.com/google/protobuf/archive/v3.3.0.tar.gz
+  tar -xzvf protobuf-3.3.0.tar.gz
+  cd protobuf-3.3.0 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached protobuf."
 fi
