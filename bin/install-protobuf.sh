52b727af2e6701756400c1cdbeffc672ca04a00f
Alena Varkockova <varkockova.a@gmail.com>
2017-12-21 10:39:04 +0100

    Try to download compiled binaries

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -e
 # check to see if protobuf folder is empty
-if [ ! -d "$HOME/protobuf/lib" ]; then
-  curl -L -o protobuf-3.3.0.tar.gz https://github.com/google/protobuf/archive/v3.3.0.tar.gz
-  tar -xzvf protobuf-3.3.0.tar.gz
-  cd protobuf-3.3.0 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
+download_url='https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_32.zip'
+unamestr=`uname`
+echo $unamestr
+if [[ "$unamestr" == 'Darwin' ]]; then
+   download_url='https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-osx-x86_64.zip'
+fi
+if [ ! -d "$HOME/protoc" ]; then
+  curl -L -o protoc-3.3.0.zip $download_url
+  unzip -d protoc-3.3.0 -a protoc-3.3.0.zip
+  mv -v ./protoc-3.3.0 ~/protoc
+  rm protoc-3.3.0.zip
 else
-  echo "Using cached protobuf."
+  echo "Using already installed protoc."
 fi
