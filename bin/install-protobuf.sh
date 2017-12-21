f4388e86e66af4bce501c65e106e54135870f94d
Alena Varkockova <varkockova.a@gmail.com>
2017-12-21 13:32:30 +0100

    Check the correct directory

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -7,7 +7,7 @@ echo $unamestr
 if [ "$unamestr" == 'Darwin' ]; then
    download_url='https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-osx-x86_64.zip'
 fi
-if [ ! -d "$HOME/protoc" ]; then
+if [ ! -d "$HOME/protobuf" ]; then
   curl -L -o protoc-3.3.0.zip $download_url
   unzip -d protoc-3.3.0 -a protoc-3.3.0.zip
   mv -v ./protoc-3.3.0 ~/protobuf
