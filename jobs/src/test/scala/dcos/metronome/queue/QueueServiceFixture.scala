b9cfc90c2dec114fbabd7f507f8054cf768f81d5
Alena Varkockova <varkockova.a@gmail.com>
2017-12-28 10:59:19 +0100

    Fix the build

--- jobs/src/test/scala/dcos/metronome/queue/QueueServiceFixture.scala
+++ jobs/src/test/scala/dcos/metronome/queue/QueueServiceFixture.scala
@@ -1,10 +1,11 @@
 package dcos.metronome.queue
 
+import dcos.metronome.queue.impl.QueueServiceImpl
 import mesosphere.marathon.core.launchqueue.LaunchQueue
 
 object QueueServiceFixture {
 
-  def simpleQueueService(): QueueService = new QueueService {
+  def simpleQueueService(): QueueService = new QueueServiceImpl(null) {
     override def list(): Iterable[LaunchQueue.QueuedTaskInfo] = List[LaunchQueue.QueuedTaskInfo]()
   }
 }
