39e0c64039f3e5b62bdaae159071919ab7fe4879
Ken Sipe <kensipe@gmail.com>
2018-01-04 14:25:50 -0600

    taking advantage of the double package

--- jobs/src/main/scala/dcos/metronome/queue/impl/LaunchQueueServiceImpl.scala
+++ jobs/src/main/scala/dcos/metronome/queue/impl/LaunchQueueServiceImpl.scala
@@ -6,9 +6,11 @@ import dcos.metronome.model.QueuedJobRunInfo
 import dcos.metronome.queue.LaunchQueueService
 import mesosphere.marathon.core.launchqueue.LaunchQueue
 
+import scala.collection.immutable.Seq
+
 class LaunchQueueServiceImpl(launchQueue: LaunchQueue) extends LaunchQueueService {
 
-  override def list(): scala.collection.immutable.Seq[QueuedJobRunInfo] = {
+  override def list(): Seq[QueuedJobRunInfo] = {
     launchQueue.list.map(_.toModel)
   }
 
--- jobs/src/test/scala/dcos/metronome/queue/QueueServiceFixture.scala
+++ jobs/src/test/scala/dcos/metronome/queue/QueueServiceFixture.scala
@@ -3,10 +3,11 @@ package queue
 
 import dcos.metronome.model.QueuedJobRunInfo
 import dcos.metronome.queue.impl.LaunchQueueServiceImpl
+import scala.collection.immutable.Seq
 
 object QueueServiceFixture {
 
   def simpleQueueService(): LaunchQueueService = new LaunchQueueServiceImpl(null) {
-    override def list(): scala.collection.immutable.Seq[QueuedJobRunInfo] = List[QueuedJobRunInfo]()
+    override def list(): Seq[QueuedJobRunInfo] = List[QueuedJobRunInfo]()
   }
 }
