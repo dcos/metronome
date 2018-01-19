784a6d524ac87a599af477878f49365c918073cc
Alena Varkockova <varkockova.a@gmail.com>
2018-01-19 14:39:59 +0100

    Fix the scripts location

--- ci/si_provision.sh
+++ ci/si_provision.sh
@@ -3,5 +3,5 @@
 # we want to share the cluster launch scripts and si_pipeline scripts with Marathon
 wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/launch_cluster.sh
 wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/si_pipeline.sh
-chmod +x launch_cluster.sh
-chmod +x si_pipeline.sh
+chmod +x ci/launch_cluster.sh
+chmod +x ci/si_pipeline.sh
