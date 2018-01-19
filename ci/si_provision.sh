81e390e5be086b76b278465a6c5194424bdf24d4
Alena Varkockova <varkockova.a@gmail.com>
2018-01-19 14:33:19 +0100

    Remove old scripts, copy to CI folder

--- ci/si_provision.sh
+++ ci/si_provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # we want to share the cluster launch scripts and si_pipeline scripts with Marathon
-wget https://raw.githubusercontent.com/mesosphere/marathon/master/ci/launch_cluster.sh
-wget https://raw.githubusercontent.com/mesosphere/marathon/master/ci/si_pipeline.sh
+wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/launch_cluster.sh
+wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/si_pipeline.sh
 chmod +x launch_cluster.sh
 chmod +x si_pipeline.sh
