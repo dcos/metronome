9260ace14ef10d623860046ae8f1dc5d31570555
Ken Sipe <ken@mesosphere.io>
2018-02-14 12:01:32 -0600

    error handling around loading data.   resolves issue is there is a dangling path with no data or with corrupt data

--- jobs/src/main/scala/dcos/metronome/repository/LoadContentOnStartup.scala
+++ jobs/src/main/scala/dcos/metronome/repository/LoadContentOnStartup.scala
@@ -2,6 +2,7 @@ package dcos.metronome
 package repository
 
 import akka.actor.{ Actor, ActorLogging, Stash }
+import mesosphere.marathon.StoreCommandFailedException
 
 import scala.concurrent.Future
 import scala.util.{ Failure, Success }
@@ -33,7 +34,7 @@ trait LoadContentOnStartup[Id, Model] extends Actor with Stash with ActorLogging
     val loadAllFuture = repo.ids().flatMap { ids =>
       ids.foldLeft(Future.successful(List.empty[Model])) {
         case (resultsFuture, id) => resultsFuture.flatMap { res =>
-          repo.get(id).map(_.map(_ :: res).getOrElse(res))
+          getModel(id).map(_.map(_ :: res).getOrElse(res))
         }
       }
     }
@@ -43,6 +44,14 @@ trait LoadContentOnStartup[Id, Model] extends Actor with Stash with ActorLogging
       case Failure(ex) =>
         log.error(ex, "Can not load initial data. Give up.")
         throw ex
+    }
+  }
+
+  private def getModel(id: Id): Future[Option[Model]] = {
+    repo.get(id).recoverWith {
+      case ex: StoreCommandFailedException =>
+        log.error(s"ID $id is a dangling path with data recovery issues.  Exception message: ${ex.getMessage}")
+        Future { None }
     }
   }
 }
