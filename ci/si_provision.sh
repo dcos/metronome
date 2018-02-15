56e884a4bb650c998442837a233ad2c557423350
Alena Varkockova <varkockova.a@gmail.com>
2018-02-15 14:06:27 +0100

    Fix our wget monorepo MARATHON-8086 (#199)

--- ci/si_provision.sh
+++ ci/si_provision.sh
@@ -3,5 +3,7 @@
 # we want to share the cluster launch scripts and si_pipeline scripts with Marathon
 wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/launch_cluster.sh
 wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/si_pipeline.sh
+wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/si_install_deps.sh
 chmod +x ci/launch_cluster.sh
 chmod +x ci/si_pipeline.sh
+chmod +x ci/si_install_deps.sh
