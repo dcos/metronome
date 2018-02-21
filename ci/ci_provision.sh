223ec4371ab11c952366adf8c73bb7e05d5879c1
Alena Varkockova <varkockova.a@gmail.com>
2018-02-21 16:51:34 +0100

    Try sudo

--- ci/ci_provision.sh
+++ ci/ci_provision.sh
@@ -2,4 +2,4 @@
 
 # we want to share the release scripts with Marathon
 wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/upgrade.sc
-apt-get install -y unzip
+sudo apt-get install -y unzip
