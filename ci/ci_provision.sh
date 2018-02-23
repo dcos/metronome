ebf1923dc34d97c339334c2721cef6db827e6ec7
Alena Varkockova <varkockova.a@gmail.com>
2018-02-23 16:55:06 +0100

    Add chmod

--- ci/ci_provision.sh
+++ ci/ci_provision.sh
@@ -3,3 +3,4 @@
 # we want to share the release scripts with Marathon
 sudo apt-get install -y wget unzip
 wget -P ./ci/ https://raw.githubusercontent.com/mesosphere/marathon/master/ci/upgrade.sc
+chmod +x ci/upgrade.sc
