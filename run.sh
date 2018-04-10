65299a5dca1f9c6d0b8939b16da9268b1c3fae7b
Alena Varkockova <varkockova.a@gmail.com>
2018-04-10 18:17:07 +0200

    Change the path and parameter

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ZK_URL="${1:-zk://127.0.0.1:2181/metronome}"
 MESOS_MASTER_URL="${2:-127.0.0.1:5050}"
-PLAY_PORT="${3:-8888}"
+HTTP_PORT="${3:-9000}"
 
 sbt universal:packageBin
 ZIP_COUNT=`ls target/universal/metronome*.zip | wc -l`
@@ -12,5 +12,5 @@ if [ ${ZIP_COUNT} -gt 1 ]; then
 fi
 # unpack the package
 unzip -o -d target/universal -a target/universal/metronome-*.zip
-chmod +x target/universal/metronome/metronome-*/bin/metronome
-LIBPROCESS_IP=127.0.0.1 ./target/universal/metronome/metronome-*/bin/metronome -d -v -Dmetronome.framework.name=metronome-dev -Dmetronome.zk.url=$ZK_URL -Dmetronome.mesos.master.url=$MESOS_MASTER_URL -Dplay.server.http.port=$PLAY_PORT
+chmod +x target/universal/metronome-*/bin/metronome
+LIBPROCESS_IP=127.0.0.1 ./target/universal/metronome-*/bin/metronome -d -v -Dmetronome.framework.name=metronome-dev -Dmetronome.zk.url=$ZK_URL -Dmetronome.mesos.master.url=$MESOS_MASTER_URL -Dplay.server.http.port=$HTTP_PORT
