74956b13e8e0febd66be45344a773a0dd13995da
Alena Varkockova <varkockova.a@gmail.com>
2018-08-17 11:34:01 +0200

    Exclude 0 from http codes

--- tests/system/wait_for_cluster.sh
+++ tests/system/wait_for_cluster.sh
@@ -3,7 +3,7 @@
 while [[ true ]];
 do
     HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL''`))
-    if [ $((HTTP_CODE)) -lt 400 ]; then
+    if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
         exit 0
     fi
     echo "Waiting for $DCOS_URL to become active, got HTTP $HTTP_CODE. Retrying..."
