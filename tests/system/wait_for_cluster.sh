c25c6fcbba9ee87b618f22ac3932a4fd31d7e703
Alena Varkockova <varkockova.a@gmail.com>
2018-08-20 13:17:11 +0200

    Wait also for metronome

--- tests/system/wait_for_cluster.sh
+++ tests/system/wait_for_cluster.sh
@@ -5,7 +5,6 @@ do
     HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL''`))
     if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
         echo "$DCOS_URL returned HTTP $HTTP_CODE. We're ready!"
-        exit 0
     fi
     echo "Waiting for $DCOS_URL to become active, got HTTP $HTTP_CODE. Retrying..."
     sleep 5;
@@ -16,8 +15,9 @@ do
     HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL/service/metronome/v1/jobs''`))
     echo "$DCOS_URL/service/metronome/v1/jobs returned HTTP $HTTP_CODE. We're ready!"
     if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
-        exit 0
     fi
     echo "Waiting for $DCOS_URL/service/metronome/v1/jobs to become active, got HTTP $HTTP_CODE. Retrying..."
     sleep 5;
 done
+
+exit 0
