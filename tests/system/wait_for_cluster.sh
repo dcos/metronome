9985b8a72d8eae2368999cd7164050dbbce4898f
Alena Varkockova <varkockova.a@gmail.com>
2018-08-20 09:20:58 +0200

    Wait for capabilities endpoint

--- tests/system/wait_for_cluster.sh
+++ tests/system/wait_for_cluster.sh
@@ -9,3 +9,13 @@ do
     echo "Waiting for $DCOS_URL to become active, got HTTP $HTTP_CODE. Retrying..."
     sleep 5;
 done
+
+while [[ true ]];
+do
+    HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL/capabilities''`))
+    if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
+        exit 0
+    fi
+    echo "Waiting for $DCOS_URL/capabilities to become active, got HTTP $HTTP_CODE. Retrying..."
+    sleep 5;
+done
