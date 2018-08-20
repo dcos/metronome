4e3ec9b1118184f98e302677822c5eeb9eda33e0
Alena Varkockova <varkockova.a@gmail.com>
2018-08-20 11:56:15 +0200

    Print out when end waiting

--- tests/system/wait_for_cluster.sh
+++ tests/system/wait_for_cluster.sh
@@ -4,6 +4,7 @@ while [[ true ]];
 do
     HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL''`))
     if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
+        echo "$DCOS_URL returned HTTP $HTTP_CODE. We're ready!"
         exit 0
     fi
     echo "Waiting for $DCOS_URL to become active, got HTTP $HTTP_CODE. Retrying..."
@@ -13,6 +14,7 @@ done
 while [[ true ]];
 do
     HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL/capabilities''`))
+    echo "$DCOS_URL/capabilities returned HTTP $HTTP_CODE. We're ready!"
     if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
         exit 0
     fi
