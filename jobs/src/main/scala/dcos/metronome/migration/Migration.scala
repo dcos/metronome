19671b1a32914d268e6ffac8003d4a20400840d6
Alena Varkockova <varkockova.a@gmail.com>
2018-08-21 10:46:20 +0200

    Pass in execution context

--- jobs/src/main/scala/dcos/metronome/migration/Migration.scala
+++ jobs/src/main/scala/dcos/metronome/migration/Migration.scala
@@ -1,9 +1,11 @@
 package dcos.metronome
 package migration
 
+import scala.concurrent.ExecutionContext
+
 /** Handles the state migration */
 trait Migration {
 
   /** This call will block until the migration completed */
-  def migrate(): Unit
+  def migrate()(implicit ec: ExecutionContext): Unit
 }
--- jobs/src/main/scala/dcos/metronome/migration/impl/MigrationImpl.scala
+++ jobs/src/main/scala/dcos/metronome/migration/impl/MigrationImpl.scala
@@ -2,23 +2,23 @@ package dcos.metronome
 package migration.impl
 
 import dcos.metronome.migration.Migration
-import dcos.metronome.repository.impl.kv.{JobHistoryPathResolver, JobRunPathResolver, JobSpecPathResolver}
-import mesosphere.util.state.{PersistentStore, PersistentStoreManagement, PersistentStoreWithNestedPathsSupport}
+import dcos.metronome.repository.impl.kv.{ JobHistoryPathResolver, JobRunPathResolver, JobSpecPathResolver }
+import mesosphere.util.state.{ PersistentStore, PersistentStoreManagement, PersistentStoreWithNestedPathsSupport }
 import org.slf4j.LoggerFactory
 
-import scala.async.Async.{async, await}
+import scala.async.Async.{ async, await }
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{ Await, ExecutionContext, Future }
 
 class MigrationImpl(store: PersistentStore) extends Migration {
   import MigrationImpl._
 
-  override def migrate(): Unit = {
+  override def migrate()(implicit ec: ExecutionContext): Unit = {
     Await.result(initializeStore(), Duration.Inf)
     log.info("Migration successfully applied for version")
   }
 
-  private[this] def initializeStore(): Future[Unit] = async {
+  private[this] def initializeStore()(implicit ec: ExecutionContext): Future[Unit] = async {
     store match {
       case store: PersistentStoreManagement with PersistentStoreWithNestedPathsSupport =>
         await(store.initialize())
