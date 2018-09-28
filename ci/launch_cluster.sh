c8954c5c6a6dc15297e31ffb3225b01073735dd9
Alena Varkockova <varkockova.a@gmail.com>
2018-09-28 03:55:03 +0200

    Pull in si pipeline scripts from Marathon (#274)

---
+++ ci/launch_cluster.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -x -e -o pipefail
+
+# Ensure dependencies are installed.
+if ! command -v jq >/dev/null; then
+    echo "jq was not found. Please install it."
+    exit 1
+fi
+if ! command -v envsubst >/dev/null 2>&1; then
+    echo "envsubst was not found. Please install along with gettext."
+    exit 1
+fi
+
+# Two parameters are expected: CHANNEL and VARIANT where CHANNEL is the respective PR and
+# VARIANT could be one of three custer variants: open, strict or permissive.
+if [ "$#" -ne 3 ]; then
+    echo "Expected 3 parameters: launch_cluster.sh <channel> <variant> <deployment-name>"
+    echo "e.g. CLI_TEST_SSH_KEY='test.pem' launch_cluster.sh 'testing/pull/1739' 'open' 'si-testing-open'"
+    exit 1
+fi
+
+CHANNEL="$1"
+VARIANT="$2"
+DEPLOYMENT_NAME="$3"
+CONFIG_PATH="$DEPLOYMENT_NAME.yaml"
+INFO_PATH="$DEPLOYMENT_NAME.info.json"
+
+if [ "$VARIANT" == "open" ]; then
+  INSTALLER="https://downloads.dcos.io/dcos/${CHANNEL}/dcos_generate_config.sh"
+else
+  INSTALLER="https://downloads.mesosphere.com/dcos-enterprise/${CHANNEL}/dcos_generate_config.ee.sh"
+fi
+
+echo "Using: ${INSTALLER}"
+
+# Create config.yaml for dcos-launch.
+envsubst <<EOF > "$CONFIG_PATH"
+---
+launch_config_version: 1
+deployment_name: $DEPLOYMENT_NAME
+installer_url: $INSTALLER
+provider: onprem
+platform: aws
+aws_region: us-west-2
+os_name: cent-os-7-dcos-prereqs
+key_helper: true
+instance_type: m4.large
+num_public_agents: 1
+num_private_agents: 3
+num_masters: 3
+dcos_config:
+    cluster_name: $DEPLOYMENT_NAME
+    resolvers:
+        - 8.8.4.4
+        - 8.8.8.8
+    dns_search: mesos
+    master_discovery: static
+    exhibitor_storage_backend: static
+    rexray_config_preset: aws
+EOF
+
+# Append license and securoty mode for EE variants.
+if [ "$VARIANT" != "open" ]; then
+    echo "    license_key_contents: $DCOS_LICENSE" >> "$CONFIG_PATH"
+    echo "    security: $VARIANT" >> "$CONFIG_PATH"
+fi
+
+# Create cluster.
+if ! pipenv run dcos-launch -c "$CONFIG_PATH" -i "$INFO_PATH" create; then
+  echo "Failed to launch a cluster via dcos-launch"
+  exit 2
+fi
+if ! pipenv run dcos-launch -i "$INFO_PATH" wait; then
+  exit 3
+fi
+
+# Extract SSH key
+jq -r .ssh_private_key "$INFO_PATH" > "$CLI_TEST_SSH_KEY"
+
+# Return dcos_url
+CLUSTER_IP="$(pipenv run dcos-launch -i "$INFO_PATH" describe | jq -r ".masters[0].public_ip")"
+echo "Launched cluster with IP $CLUSTER_IP"
+echo "$CLUSTER_IP"
---
+++ ci/si_pipeline.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -x +e -o pipefail
+
+# Two parameters are expected: CHANNEL and VARIANT where CHANNEL is the respective PR and
+# VARIANT could be one of three custer variants: open, strict or permissive.
+if [ "$#" -ne 2 ]; then
+    echo "Expected 2 parameters: <channel> and <variant> e.g. si.sh testing/pull/1739 open"
+    exit 1
+fi
+
+CHANNEL="$1"
+VARIANT="$2"
+
+JOB_NAME_SANITIZED=$(echo "$JOB_NAME" | tr -c '[:alnum:]-' '-')
+DEPLOYMENT_NAME="$JOB_NAME_SANITIZED-$BUILD_NUMBER"
+INFO_PATH="$DEPLOYMENT_NAME.info.json"
+ROOT_PATH=$(pwd)
+
+# Change work directory to ./tests
+cd tests/system || exit 1
+
+function create-junit-xml {
+    local testsuite_name=$1
+    local testcase_name=$2
+    local error_message=$3
+
+	cat > "$ROOT_PATH/shakedown.xml" <<-EOF
+	<testsuites>
+	  <testsuite name="$testsuite_name" errors="0" skipped="0" tests="1" failures="1">
+	      <testcase classname="$testsuite_name" name="$testcase_name">
+	        <failure message="test setup failed">$error_message</failure>
+	      </testcase>
+	  </testsuite>
+	</testsuites>
+	EOF
+}
+
+function exit-with-cluster-launch-error {
+    echo "$1"
+    create-junit-xml "dcos-launch" "cluster.create" "$1"
+    pipenv run dcos-launch -i "$INFO_PATH" delete
+    "$ROOT_PATH/ci/dataDogClient.sc" "marathon.build.$JOB_NAME_SANITIZED.cluster_launch.failure" 1
+    exit 0
+}
+
+function download-diagnostics-bundle {
+	BUNDLE_NAME="$(pipenv run dcos node diagnostics create all | grep -oE 'bundle-.*')"
+	echo "Waiting for bundle ${BUNDLE_NAME} to be downloaded"
+	STATUS_OUTPUT="$(pipenv run dcos node diagnostics --status)"
+	while [[ $STATUS_OUTPUT =~ "is_running: True" ]]; do
+		echo "Diagnostics job still running, retrying in 5 seconds."
+		sleep 5
+		STATUS_OUTPUT="$(pipenv run dcos node diagnostics --status)"
+	done
+	pipenv run dcos node diagnostics download "${BUNDLE_NAME}" --location=./diagnostics.zip
+}
+
+# Install dependencies and expose new PATH value.
+# shellcheck source=../../ci/si_install_deps.sh
+source "$ROOT_PATH/ci/si_install_deps.sh"
+
+# Launch cluster and run tests if launch was successful.
+CLI_TEST_SSH_KEY="$(pwd)/$DEPLOYMENT_NAME.pem"
+export CLI_TEST_SSH_KEY
+
+if [ "$VARIANT" == "strict" ]; then
+  DCOS_URL="https://$( "$ROOT_PATH/ci/launch_cluster.sh" "$CHANNEL" "$VARIANT" "$DEPLOYMENT_NAME" | tail -1 )"
+  wget --no-check-certificate -O fixtures/dcos-ca.crt "$DCOS_URL/ca/dcos-ca.crt"
+else
+  DCOS_URL="http://$( "$ROOT_PATH/ci/launch_cluster.sh" "$CHANNEL" "$VARIANT" "$DEPLOYMENT_NAME" | tail -1 )"
+fi
+
+CLUSTER_LAUNCH_CODE=$?
+export DCOS_URL
+case $CLUSTER_LAUNCH_CODE in
+  0)
+      "$ROOT_PATH/ci/dataDogClient.sc" "marathon.build.$JOB_NAME_SANITIZED.cluster_launch.success" 1
+      cp -f "$DOT_SHAKEDOWN" "$HOME/.shakedown"
+      timeout --preserve-status -s KILL 2h make test
+      SI_CODE=$?
+      if [ ${SI_CODE} -gt 0 ]; then
+        "$ROOT_PATH/ci/dataDogClient.sc" "marathon.build.$JOB_NAME_SANITIZED.failure" 1
+        download-diagnostics-bundle
+      else
+        "$ROOT_PATH/ci/dataDogClient.sc" "marathon.build.$JOB_NAME_SANITIZED.success" 1
+      fi
+      pipenv run dcos-launch -i "$INFO_PATH" delete || true
+      exit "$SI_CODE" # Propagate return code.
+      ;;
+  2) exit-with-cluster-launch-error "Cluster launch failed.";;
+  3) exit-with-cluster-launch-error "Cluster did not start in time.";;
+  *) exit-with-cluster-launch-error "Unknown error in cluster launch: $CLUSTER_LAUNCH_CODE";;
+esac
