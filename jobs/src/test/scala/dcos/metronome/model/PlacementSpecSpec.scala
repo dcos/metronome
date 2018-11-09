d12e2fc47c7d6e56159042f96e2460c5e287ee1e
Ken Sipe <ken@mesosphere.io>
2018-11-09 12:10:19 -0600

    fixing Some(x) == x issue for test results

--- jobs/src/test/scala/dcos/metronome/model/PlacementSpecSpec.scala
+++ jobs/src/test/scala/dcos/metronome/model/PlacementSpecSpec.scala
@@ -1,10 +1,9 @@
 package dcos.metronome.model
 
-import dcos.metronome.utils.test.Mockito
 import org.scalatest.{ FunSuite, Matchers }
 
 class PlacementSpecSpec extends FunSuite with Matchers {
   test("Operator unapply converts EQ to IS") {
-    Operator.unapply("EQ").shouldEqual(Operator.Is)
+    Operator.unapply("EQ").get.shouldEqual(Operator.Is)
   }
 }
