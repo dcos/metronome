7e04c483fdc5eab4b7574e6daee70809c98a93e9
Tarun Gupta Akirala <takirala@users.noreply.github.com>
2019-06-26 23:07:43 +0530

    Investigate failing tests

--- tests/system/wait_for_cluster.sh
+++ tests/system/wait_for_cluster.sh
@@ -2,7 +2,7 @@
 
 while [[ true ]];
 do
-    HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL''`))
+    HTTP_CODE=$((`curl -s -o /dev/null -w ''%{http_code}'' ''$DCOS_URL/dcos-metadata/dcos-version.json''`))
     if [[ $((HTTP_CODE)) -lt 400 && $((HTTP_CODE)) -ne 0 ]]; then
         echo "$DCOS_URL returned HTTP $HTTP_CODE. We're ready!"
         exit 0;
