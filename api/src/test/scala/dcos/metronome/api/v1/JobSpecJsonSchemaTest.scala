a4d46e85d7808f5e133cf0c377fd6d0baad5bdbb
Tim Harper <tharper@mesosphere.io>
2020-03-12 09:43:38 -0600

    Optimize jobspec schema regular expression (#367)
    * Protobuf installation is handled by sbt
    * Also, add property checks
    
    JIRA Issues: MARATHON-8730

---
+++ api/src/test/scala/dcos/metronome/api/v1/JobSpecJsonSchemaTest.scala
@@ -0,0 +1,54 @@
+package dcos.metronome.api.v1
+
+import com.eclipsesource.schema.SchemaValidator
+import dcos.metronome.api.v1.models.schema
+import org.scalatest.{ FunSuite, Matchers }
+import dcos.metronome.utils.test.Mockito
+import org.scalatest.prop.TableDrivenPropertyChecks
+import play.api.libs.json._
+
+class JobSpecJsonSchemaTest extends FunSuite with Mockito with Matchers with TableDrivenPropertyChecks {
+  val template = Json.obj(
+    "id" -> "test",
+    "run" -> Json.obj(
+      "cpus" -> 1,
+      "disk" -> 0,
+      "mem" -> 32))
+
+  val jobSpecIds = Table(
+      ("id", "valid"),
+      ("a", true),
+      ("a-a", true),
+      ("a-a.a-a", true),
+      ("a.a.a", true),
+      ("a1a.a1.a1", true),
+      ("a1234567890-a1234567890.a1234567890-a1234567890.a", true),
+      ("a-", false),
+      ("a.", false),
+      ("a1.a-", false),
+      ("a1.a1.", false),
+      ("-a", false),
+      (".a", false),
+      ("-a1.a", false),
+      ("a1-.a", false),
+      (".a1.a1", false),
+
+      // invalid characters
+      ("a?.a", false),
+      ("a?a.a", false),
+      ("a.a?", false),
+      ("a.a?a", false),
+  )
+
+  val schemaValidator = SchemaValidator()
+
+  test(s"id should be validated") {
+    forAll(jobSpecIds) { (id, shouldSucceed) =>
+      val updated = template + ("id", JsString(id))
+      val result = schemaValidator.validate(schema.JobSpecSchema.schemaType, updated)
+      withClue(result.toString) {
+        result.isSuccess shouldBe (shouldSucceed)
+      }
+    }
+  }
+}
